f323790a80e21ef576d8d75161deb96e4aa74982
Lgood16 <Loganmgood@gmail.com>
2026-09-02 16:51:43 -0700

    commit

--- Class Examples/Assets/Module 2 - GameObjects and Transforms/Scripts/InputExample.cs
+++ Class Examples/Assets/Module 2 - GameObjects and Transforms/Scripts/InputExample.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
 
+
 public class InputExample : MonoBehaviour
 {
+[SerializeField]
+private bool pressed = false;
 
     public void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("The Player is pressing A!");
+        string keyPressed = Input.inputString;
+        switch (keyPressed) {
+            case "a":
+                transform.position += new Vector3(0.1f, 0, 0);
+                break;
+            case "d":
+                transform.position += new Vector3(-0.1f, 0, 0);
+                break;
+            case "w":
+                transform.position += new Vector3(0, 0.0f, 0.1f);
+                break;
+            case "s":
+                transform.position += new Vector3(0, 0.0f, -0.1f);
+                break;
         }
+
+        
     }
 }
